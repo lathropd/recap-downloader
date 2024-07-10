6219d7b92da6ad3fc720d2009b796a13d03d90c0
Daniel Lathrop <daniel.lathrop@gmail.com>
2024-07-10 16:39:59 -0400

    muti-threaded downloads, eliminate additional metadata types

--- main.go
+++ main.go
@@ -33,20 +33,21 @@ func main () {
   // can get unauthenticated of main docket info
   COURT_LISTENER_API := "https://www.courtlistener.com/api/rest/v3/dockets/"
   downloadDir := findUserFolder("Downloads")
+  //fmt.Println(downloadDir)
   //fmt.Println(API_HEADER)
 
   // fmt.Println(os.Args)
-  var case_spec, case_id string
+  var caseSpec, caseId string
 
   switch {
     case len(os.Args) > 1: {
 
       // fmt.Println("Reading case id from parameter")
-      case_spec = os.Args[1]
+      caseSpec = os.Args[1]
     }
     default: {
       fmt.Print("Enter the case's courtlistener.com url: ")
-      fmt.Scanln(&case_spec)
+      fmt.Scanln(&caseSpec)
     }
 
 
@@ -54,21 +55,20 @@ func main () {
 
   fmt.Println("Attempting to find case")
   re := regexp.MustCompile(`\d+`)
-  case_id = re.FindString(case_spec)
+  caseId = re.FindString(caseSpec)
 
   fmt.Println("Searching CourtListener API ...")
 
 
   // Create REST client
   client := resty.New()
-
   caseData := ApiResult{}
 
 
   resp, err := client.R().
   EnableTrace().
   SetResult(&caseData).
-  Get(COURT_LISTENER_API + case_id + "/")
+  Get(COURT_LISTENER_API + caseId + "/")
 
 
   resp.Status()
@@ -87,53 +87,58 @@ func main () {
   var caseYear string
 
   if caseData.DateFiled != "" {
-    caseYear = caseDatate -output recap_downloader_universal recap_downloader_arm64 recap_downloader_amd64
-    :!
-    .DateFiled[2:4]
+    caseYear = caseData.DateFiled[2:4]
   } else {
     fmt.Println("Sorry, getting back invalid case data")
     os.Exit(1)
   }
-
+  
+  pacerCaseId := string(caseData.PacerCaseId)
 
   fmt.Println("Found", caseData.CaseName)
   fmt.Println("************** Downloading available PDFs/media from the Internet Archive ***************")
 
   c := colly.NewCollector(
+    colly.Async(),
   )
+
+  c.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 4})
 
 
   c.OnRequest(func(r *colly.Request) {
-    fmt.Print(".")
   })
 
   c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-    e.Request.Visit(e.Attr("href"))
+    url := e.Attr("href")
+    dotArray := strings.Split(url, ".")
+    fileExtension := dotArray[len(dotArray) -1]
+    junkFileTypes := []string{ "sqlite", "sql", "torrent", "xml", "json", "gz", "html", "txt", "zip", "png" }
+    
+    if slices.Index(junkFileTypes, fileExtension) == -1 {
+
+      e.Request.Visit(e.Attr("href"))
+    }
   })
 
   c.OnScraped(func(r *colly.Response) { 
+    fmt.Print(".")
     url := r.Request.URL.String()
     urlArray := strings.Split(url, "/")
     fileName := urlArray[len(urlArray) - 1]
 
-    dotArray := strings.Split(url, ".")
-    fileExtension := dotArray[len(dotArray) -1]
-    junkFileTypes := []string{ "sqlite", "sql", "torrent" }
-
-    if slices.Index(junkFileTypes, fileExtension) == -1 {
-      r.Save(path.Join(downloadDir, fileName))
-    }
+    r.Save(path.Join(downloadDir, fileName))
 
 
   })
 
 
   c.Visit("https://ia802709.us.archive.org/" +
-  caseYear +
-  "/items/gov.uscourts." + 
-  caseData.CourtId + "." + 
-  string(caseData.PacerCaseId))
+    caseYear +
+    "/items/gov.uscourts." + 
+    caseData.CourtId + "." + 
+    pacerCaseId)
 
+  c.Wait()
 }
 
 
@@ -148,6 +153,9 @@ func findUserFolder(targetDirName string) string {
   // depends only on the stdlib os module and some general knowledge about directories
 
   // throws an error if it fails
+
+
+  // right now just does the naive, stupid thing
   userHomeDir, err := os.UserHomeDir()
 
   if err != nil {
@@ -156,5 +164,4 @@ func findUserFolder(targetDirName string) string {
   } 
 
  return path.Join(userHomeDir, targetDirName)
-
 }
